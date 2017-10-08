a38c9396c6c8558e9553bbc65cdef7b7da6d8981
Lyndsey Ferguson <lyndsey.ferguson@gmail.com>
2017-10-08 18:05:16 -0400

    initial reducer

--- App.js
+++ App.js
@@ -1,15 +1,85 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {
+  Component
+} from 'react';
+import {
+  AppState,
+  AsyncStorage,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import {
+  applyMiddleware,
+  createStore
+} from 'redux'
+import { createLogger } from 'redux-logger'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import appReducer from './reducers/AppReducer'
+
+const logger = createLogger();
+const createStoreWithMiddleware = applyMiddleware(thunk, logger)(createStore);
+const store = createStoreWithMiddleware(appReducer);
+
+if (module.hot) {
+  module.hot.accept(() => {
+    const nextRootReducer = require('./reducers/AppReducer').default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoadingStore: false
+    }
+  }
+  componentWillMount() {
+    var self = this;
+    this.setState({isStoreLoading: true});
+    AppState.addEventListener('change', this._handleAppStateChange.bind(this));
+    AsyncStorage.getItem('SmartComponentsApp').then((value)=>{
+      if(value && value.length){
+        let initialStore = JSON.parse(value);
+        self.setState({store: createStore(rootReducer, initialStore, applyMiddleware(thunk, logger))});
+      }else{
+        self.setState({store: store});
+      }
+      self.setState({isStoreLoading: false});
+    }).catch((error)=>{
+      self.setState({store: store});
+      self.setState({isStoreLoading: false});
+    })
+  }
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this._handleAppStateChange.bind(this));
+  }
+  _handleAppStateChange(currentAppState) {
+    let storingValue = JSON.stringify(this.state.store.getState())
+    AsyncStorage.setItem('SmartComponentsApp', storingValue);
+  }
   render() {
+    if (this.state.isStoreLoading) {
+      return (
+        <View style={styles.container}>
+          <StatusBar hidden={true}/>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+      );
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <Provider store={this.state.store}>
+        <View>
+          <StatusBar hidden={true}/>
+          <Text>Loaded</Text>
+        </View>
+      </Provider>
+    )
   }
 }
 
---
+++ reducers/AppReducer.js
@@ -0,0 +1,17 @@
+import { combineReducers } from 'redux';
+
+const initialState = {
+
+};
+
+const appReducer = (state = initialState, action) => {
+  switch(action.type) {
+    default: {
+      return state;
+    }
+  }
+}
+
+export default combineReducers({
+  app: appReducer
+});
